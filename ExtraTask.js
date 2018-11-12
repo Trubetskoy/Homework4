025cb474401f73eecf38c22ca84de13112233cfb
Trubetskoy <42418728+Trubetskoy@users.noreply.github.com>
2018-11-12 21:20:06 +0200

    new functions and object beginning ready to check new ExtraTask

---
+++ ExtraTask.js
@@ -0,0 +1,29 @@
+function MatrixCoordinats(r, c, r0, c0) {
+    this.arrayRow = [];
+    this.arrayColomn = [];
+    this.Rezults = [];
+    this.steps = Math.max(r,c)-1;
+    console.log(steps);
+
+    Rezults.push([r0, c0]);
+
+    for (let i = 0; i <= r; i++) {
+        arrayRow.push(i);
+    }
+    console.log(arrayRow);
+
+    for (let i = 0; i <= c; i++) {
+        arrayColomn.push(i);
+    }
+    console.log(arrayColomn);
+    console.log(Rezults);
+
+    for (let i = 2; i <= 2; i++){
+
+    }
+}
+
+MatrixCoordinats(8, 10, 6, 1);
+
+
+
